685387fdb24a2a108495d83e2cf1ad4346696057
matt <javascript.12.03.2024@gmail.com>
2025-02-11 13:44:44 +0000

    save text

--- random-chars.js
+++ random-chars.js
@@ -23,8 +23,18 @@
             
       });
       
+      navigator.clipboard.writeText(str);
       console.log(str);
 
+/*
+      document.createElement('div');
+      div.style.cssText   = ` position:absolute;left:0;right:0;width:100px;height:50px;
+                              border:1px solid blue;border-radius:5px;
+                              display:flex;justify-content:center;align-items:center
+                            `;
+      document.body.append(div);
+*/      
+      
       
       function char(){
         
